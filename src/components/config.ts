4459af2e91c388bdbf1302d0d980c787b99eaf24
nkita <nkitam7@gmail.com>
2024-03-20 23:15:17 +0900

    🚧 wip: hotkeysをわけるようにコード修正

---
+++ src/components/config.ts
@@ -0,0 +1,4 @@
+export const keymap = {
+    'up': ['j', 'ArrowUp'],
+    'down': ['k', 'ArrowDown'],
+}
--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useRef, createRef, RefObject, useEffect, forwardRef, InputHTMLAttributes, MouseEvent, ChangeEvent, } from "react"
-import { useHotkeys } from "react-hotkeys-hook"
+import { useHotkeys, useRecordHotkeys } from "react-hotkeys-hook"
 export const Todo = () => {
     const [key, setKey] = useState("")
     const [todos, setTodos] = useState(['apple', 'banana', 'orange', 'lemon', 'grape'])
@@ -8,57 +8,77 @@ export const Todo = () => {
     const todosInputRefs = useRef<RefObject<HTMLInputElement>[]>([])
     const [currentFocus, setCurrentFocus] = useState<number>(0)
     const [mode, setMode] = useState('normal')
+    const [keys, { start, stop, isRecording }] = useRecordHotkeys()
+
     useEffect(() => {
         todos.forEach((_, index) => {
             todosRefs.current[index] = createRef<HTMLButtonElement>()
             todosInputRefs.current[index] = createRef<HTMLInputElement>()
         })
         return () => { }
-    }, [])
-    useHotkeys(['h', 'j', 'k', 'l', 'e', 'Enter', 'Esc'], (e) => {
+    }, [todos])
+    const enabled = {
+        normal: { enabled: mode === "normal", enableOnContentEditable: true, enableOnFormTags: false },
+        edit: { enabled: mode === "edit", enableOnContentEditable: true, enableOnFormTags: true },
+        always: { enabled: true, enableOnContentEditable: true, enableOnFormTags: true }
+    }
+    /**
+     * Normal mode
+     */
+    // check press key name
+    useHotkeys('*', (e) => {
         setKey(e.key)
-        if (mode === "edit") {
-            switch (e.key) {
-                case 'Escape':
-                    setMode('normal')
-                    const _ref = todosRefs.current[currentFocus].current
-                    if (_ref) _ref.focus()
-                    break;
-            }
+    }, enabled.always)
 
-        } else {
-            switch (e.key) {
-                case 'j':
-                    if (currentFocus < todos.length - 1) {
-                        setFocus(currentFocus + 1)
-                    }
-                    break;
-                case 'k':
-                    if (0 < currentFocus) {
-                        setFocus(currentFocus - 1)
-                    }
-                    break;
-                case 'e':
-                    setMode('edit')
-                    const _ref = todosInputRefs.current[currentFocus].current
-                    if (_ref) {
-                        _ref.focus()
-                        const end = _ref.value.length
-                        _ref.setSelectionRange(end, end)
-                        // preventDeafultしておかないと、そのままinputエリアに文字が入力されてしまうため
-                        e.preventDefault()
-                    }
-                    break;
-                case 'Escape':
-                    setMode('normal')
-                    break;
-            }
+    // move to up 
+    useHotkeys(['k', 'ArrowUp'], (e) => {
+        setKey(e.key)
+        if (mode !== "edit" && 0 < currentFocus) setFocus(currentFocus - 1)
+    }, enabled.normal)
+
+    // move to down
+    useHotkeys(['j', 'ArrowDown'], (e) => {
+        setKey(e.key)
+        if (mode !== "edit" && currentFocus < todos.length - 1) setFocus(currentFocus + 1)
+    }, enabled.normal)
+
+    // change mode to edit
+    useHotkeys(['e', 'Enter'], (e) => {
+        setKey(e.key)
+        setMode('edit')
+        const _ref = todosInputRefs.current[currentFocus].current
+        if (_ref) {
+            _ref.focus()
+            const end = _ref.value.length
+            _ref.setSelectionRange(end, end)
+            // preventDeafultしておかないと、そのままinputエリアに文字が入力されてしまうため
+            e.preventDefault()
         }
-    }, { enabled: true, enableOnContentEditable: true, enableOnFormTags: true })
+    }, enabled.normal)
+
+    // Insert a task  
+    useHotkeys(['i'], (e) => {
+        setKey(e.key)
+    }, enabled.normal)
+
+    // Insert a task to Top
+    useHotkeys(['shift+i'], (e) => {
+        setKey(e.key)
+        alert("shift+i")
+    }, enabled.normal)
+
+    /**
+     * Edit mode
+     */
+    // change mode to normal 
+    useHotkeys(['Esc'], (e) => {
+        setKey(e.key)
+        setMode('normal')
+        const _ref = todosRefs.current[currentFocus].current
+        if (_ref) _ref.focus()
+    }, enabled.edit)
 
 
-    const handleFocus = (index: number) => setCurrentFocus(index)
-    const setFocus = (index: number) => todosRefs.current[index].current?.focus()
     const handleTodoChange = (e: ChangeEvent) => {
         todosInputRefs.current[currentFocus].current?.focus()
         const val = todosInputRefs.current[currentFocus].current?.value
@@ -67,15 +87,14 @@ export const Todo = () => {
         }
     }
 
-    const handleMainMouseDown = (e: MouseEvent<HTMLDivElement>) => {
-        e.preventDefault()
-        // if (e.target ? === "main") {
-        // }
+    const handleFocus = (index: number) => setCurrentFocus(index)
+    const setFocus = (index: number) => todosRefs.current[index].current?.focus()
+    const handleMainMouseDown = (e: MouseEvent<HTMLDivElement>) => e.preventDefault()
+    const handleTodoMouseDown = (e: MouseEvent<HTMLDivElement>) => e.stopPropagation(); // マウスダウンイベントの伝搬を停止
+    const handleBlur = () => {
+        setMode('normal')
+        setFocus(currentFocus)
     }
-
-    const handleTodoMouseDown = (e: MouseEvent<HTMLDivElement>) => {
-        e.stopPropagation(); // マウスダウンイベントの伝搬を停止
-    };
     return (
         <div className="flex flex-col w-full h-screen" id="main" onMouseDown={handleMainMouseDown}>
             <div onMouseDown={handleTodoMouseDown}>
@@ -94,8 +113,9 @@ export const Todo = () => {
                             </div>
                             <div className={`overflow-hidden ${currentFocus === index && mode === "edit" ? "w-full" : "w-0"}`}>
                                 <input
-                                    className={`focus:bg-gray-300 truncate`}
-                                    type="text" defaultValue={t} ref={todosInputRefs.current[index]} onChange={handleTodoChange} onBlur={_ => setMode('normal')} />
+                                    tabIndex={-1}
+                                    className={`focus:bg-gray-300 truncate w-full`}
+                                    type="text" defaultValue={t} ref={todosInputRefs.current[index]} onChange={handleTodoChange} onBlur={handleBlur} />
                             </div>
                         </div>
                     )

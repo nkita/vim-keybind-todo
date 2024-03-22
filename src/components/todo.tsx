acf420c884b38ec8c4ade46d5d9064d1bca96d30
nkita <nkitam7@gmail.com>
2024-03-22 20:07:22 +0900

    ✨ feat: Todoの追加機能

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,11 +1,22 @@
 'use client'
-import { useState, MouseEvent, ChangeEvent, } from "react"
+import { useState, MouseEvent, ChangeEvent, useEffect, } from "react"
 import { useHotkeys, } from "react-hotkeys-hook"
 import { useForm } from "react-hook-form"
+interface Todo {
+    id: number,
+    isCompletion?: boolean
+    priority?: string    // a character. A-Z Uppercase
+    completionDate?: string    // format yyyy-mm-dd
+    creationDate?: string    // format yyyy-mm-dd
+    text: string
+    project?: string    // +projctname
+    context?: string    // @context    
+}
+
 export const Todo = () => {
     const [key, setKey] = useState("")
-    const [todos, setTodos] = useState(['apple', 'banana', 'orange', 'lemon', 'grape'])
-    const [currentFocus, setCurrentFocus] = useState<number>(0)
+    const [todos, setTodos] = useState<Todo[]>([{ id: 0, text: 'new task1' }])
+    const [currentIndex, setCurrentIndex] = useState<number>(0)
     const [mode, setMode] = useState('normal')
     const [log, setLog] = useState("")
     const { register, setFocus } = useForm()
@@ -15,30 +26,64 @@ export const Todo = () => {
         command: { enabled: mode === "command", enableOnContentEditable: true, enableOnFormTags: true },
         always: { enabled: true, enableOnContentEditable: true, enableOnFormTags: true }
     }
+    useEffect(() => {
+        if (mode === 'edit') setFocus(`edit-${todos[currentIndex].id}`)
+        if (mode === 'normal') setFocus(`text-${todos[currentIndex].id}`)
+    }, [todos, mode, currentIndex, setFocus])
     /**
      * Normal mode
      */
     // move to up 
     useHotkeys(['k', 'ArrowUp', 'ctrl+p'], (e) => {
         e.preventDefault()
-        setKey(e.key)
-        if (mode !== "edit" && 0 < currentFocus) setFocus(`text-${currentFocus - 1}`)
+        if (0 < currentIndex) setCurrentIndex(currentIndex - 1)
     }, enabled.normal)
 
     // move to down
     useHotkeys(['j', 'ArrowDown', 'ctrl+n'], (e) => {
         e.preventDefault()
-        setKey(e.key)
-        if (mode !== "edit" && currentFocus < todos.length - 1) setFocus(`text-${currentFocus + 1}`)
-        e.preventDefault()
+        if (currentIndex < todos.length - 1) setCurrentIndex(currentIndex + 1)
     }, enabled.normal)
 
-    // change mode to edit
+    // insert task 
+    useHotkeys(['i'], (e) => {
+        e.preventDefault()
+        const newId = todos.length === 0 ? 1 : Math.max(...todos.map((t: Todo) => t.id)) + 1
+        setTodos([...todos.slice(0, currentIndex), { id: newId, text: "" }, ...todos.slice(currentIndex)])
+        setMode('edit')
+    }, enabled.normal)
+
+    // add task to Top
+    useHotkeys(['shift+i'], (e) => {
+        e.preventDefault()
+        const newId = todos.length === 0 ? 1 : Math.max(...todos.map((t: Todo) => t.id)) + 1
+        setTodos([{ id: newId, text: "" }, ...todos])
+        setCurrentIndex(0)
+        setMode('edit')
+    }, enabled.normal)
+
+    // append task 
+    useHotkeys(['a'], (e) => {
+        e.preventDefault()
+        const newId = todos.length === 0 ? 1 : Math.max(...todos.map((t: Todo) => t.id)) + 1
+        setTodos([...todos.slice(0, currentIndex + 1), { id: newId, text: "" }, ...todos.slice(currentIndex + 1)])
+        setCurrentIndex(currentIndex + 1)
+        setMode('edit')
+    }, enabled.normal)
+
+    // append task to bottom
+    useHotkeys(['shift+a'], (e) => {
+        e.preventDefault()
+        const newId = todos.length === 0 ? 1 : Math.max(...todos.map((t: Todo) => t.id)) + 1
+        setTodos([...todos, { id: newId, text: "" }])
+        setCurrentIndex(todos.length)
+        setMode('edit')
+    }, enabled.normal)
+
+    // change to edit mode
     useHotkeys(['Enter'], (e) => {
         e.preventDefault()
         setMode('edit')
-        setFocus(`edit-${currentFocus}`)
-        // preventDeafultしておかないと、そのままinputエリアに文字が入力されてしまうため
     }, enabled.normal)
 
 
@@ -55,10 +100,7 @@ export const Todo = () => {
     // change mode to normal 
     useHotkeys(['Esc', 'Enter'], (e) => {
         e.preventDefault()
-        if (!e.isComposing) {
-            setFocus(`text-${currentFocus}`)
-            setMode('normal')
-        }
+        if (!e.isComposing) setMode('normal')
     }, enabled.edit)
 
 
@@ -86,41 +128,47 @@ export const Todo = () => {
     const handleTodoChange = (e: ChangeEvent<HTMLInputElement>) => {
         const val = e.target?.value
         if (val) {
-            setTodos(todos.map((t, i) => i === currentFocus ? val : t))
+            setTodos(todos.map((t, i) => {
+                if (i === currentIndex) t.text = val
+                return t
+            }))
         }
     }
 
-    const handleFocus = (index: number) => setCurrentFocus(index)
+
+
+    const handleFocus = (index: number) => setCurrentIndex(index)
     const handleMainMouseDown = (e: MouseEvent<HTMLDivElement>) => e.preventDefault()
     const handleTodoMouseDown = (e: MouseEvent<HTMLDivElement>) => e.stopPropagation(); // マウスダウンイベントの伝搬を停止
     const handleBlur = () => {
         setMode('normal')
-        setFocus(`text-${currentFocus}`)
+        setFocus(`text-${todos[currentIndex].id}`)
     }
     return (
         <div className="flex flex-col w-full h-screen" id="main" onMouseDown={handleMainMouseDown}>
             <div onMouseDown={handleTodoMouseDown}>
                 {todos.map((t, index) => {
                     return (
-                        <div key={`${index}`} className="flex border">
-                            <div className={`overflow-hidden  ${!(currentFocus === index && mode === "edit") ? "w-full" : "w-0"}`}>
+                        <div key={t.id} className="flex border">
+                            <div className={`overflow-hidden focus-within:bg-blue-500  ${!(currentIndex === index && mode === "edit") ? "w-full" : "w-0"}`}>
                                 <button
-                                    className={`focus:bg-blue-500 w-full text-left truncate`}
+                                    className={`w-full text-left truncate`}
                                     onFocus={_ => handleFocus(index)}
-                                    autoFocus={currentFocus === index}
-                                    {...register(`text-${index}`)}
+                                    autoFocus={currentIndex === index}
+                                    {...register(`text-${t.id}`)}
                                 >
-                                    {t}
+                                    {t.text}
                                 </button>
                             </div>
-                            <div className={`overflow-hidden ${currentFocus === index && mode === "edit" ? "w-full" : "w-0"}`}>
+                            <div className={`overflow-hidden ${currentIndex === index && mode === "edit" ? "w-full" : "w-0"}`}>
                                 <input
                                     tabIndex={-1}
                                     className={`focus:bg-gray-300 truncate w-full`}
-                                    type="text" defaultValue={t}
-                                    {...register(`edit-${index}`)}
+                                    type="text"
+                                    placeholder="please input your task"
+                                    {...register(`edit-${t.id}`, { value: t.text })}
                                     onChange={handleTodoChange}
-                                    onFocus={e => e.currentTarget.setSelectionRange(t.length, t.length)}
+                                    onFocus={e => e.currentTarget.setSelectionRange(t.text.length, t.text.length)}
                                     onBlur={handleBlur} />
                             </div>
                         </div>
@@ -129,7 +177,7 @@ export const Todo = () => {
                 })}
             </div>
             <span>press:{key}</span>
-            <span>current index:{currentFocus}</span>
+            <span>current index:{currentIndex}</span>
             <span>current mode:{mode}</span>
             <span>current log:{log}</span>
         </div >

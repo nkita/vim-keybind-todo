2ab1e4078cb0bfbd749a3a23277ca6313e0729d4
nkita <nkitam7@gmail.com>
2024-05-03 09:53:36 +0900

    ✨ feat: 詳細画面のデザイン修正

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -1,6 +1,6 @@
 import { TodoProps } from "@/types"
 import { getTimeAgo } from "@/lib/time"
-import { FaArrowUpZA, FaRegCircle, FaCircleCheck, FaTag, FaSitemap, FaList, FaRegCircleCheck } from "react-icons/fa6";
+import { FaArrowUpZA, FaRegCircle, FaCircleCheck, FaTag, FaSitemap, FaList, FaRegCircleCheck, FaCircle } from "react-icons/fa6";
 export const Detail = ({
     todo
 }: {
@@ -12,9 +12,10 @@ export const Detail = ({
     const compDate = todo["completionDate"]
     const compDateLabel = compDate ? getTimeAgo(new Date(compDate)) : ""
     return (
-        <div className="p-4 w-full border rounded-md shadow-lg bg-white">
-            <ul>
-                <li className="flex font-bold items-center gap-2">{todo["isCompletion"] && <FaCircleCheck className="text-green-500 scale-125" />}{todo["text"]}</li>
+        <div className="p-4 w-full border rounded-md bg-white border-primary/90">
+            <h2 className="text-primary/80 font-medium text-center py-4">詳細</h2>
+            <ul className="flex flex-col gap-2">
+                <li className="flex font-bold items-center gap-2"><span className="w-5 h-5 flex items-center">{todo["isCompletion"] ? <FaCircleCheck className="text-green-500 w-5 h-5" /> : <FaRegCircle className="w-5 h-5" />}</span>{todo["text"]}</li>
                 <li className="text-sm flex justify-between  text-gray-500" ><span>{creationDate && `${creationDateLabel}に作成`}</span><span> {compDate && `${compDateLabel}に完了`}</span></li>
             </ul>
         </div>

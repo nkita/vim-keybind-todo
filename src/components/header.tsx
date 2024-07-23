3123f0f3b4f360ce128a6cd272ff23bfc23fedb1
nkita <nkitam7@gmail.com>
2024-07-23 21:57:22 +0900

    feat: save

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -32,17 +32,16 @@ import {
 import { Button } from "./ui/button";
 import { Spinner } from "./ui/spinner";
 import * as React from "react";
+import { CircleCheck, CloudUpload, SaveAllIcon } from "lucide-react";
 
-export default function Header({ list, isSave }: { list: any, isSave: boolean }) {
+export default function Header({ list, isSave, isUpdate }: { list: any, isSave: boolean, isUpdate: boolean }) {
     const { loginWithRedirect, logout, user, isLoading, getAccessTokenSilently } = useAuth0();
-    const [open, setOpen] = React.useState(false)
     const [addList, setAddList] = React.useState(false)
     return (
         <div className="flex justify-between items-center w-full py-3 px-4">
             <div className="flex items-center gap-2 h-9">
                 <h1 className="border p-1 rounded-md bg-primary text-primary-foreground">Fast Todo</h1>
             </div>
-            <div className="text-sm">{isSave ? "保存中です・・・" : "保存しました"}</div>
             <div className="gap-1 hidden">
                 {addList ? (
                     <input type="text" placeholder="新しいタスクを追加" className={`p-1`}></input>
@@ -67,46 +66,50 @@ export default function Header({ list, isSave }: { list: any, isSave: boolean })
                 !user ? (
                     <Button onClick={_ => loginWithRedirect()} size={"sm"} variant={"default"}>Log In</Button>
                 ) : (
-                    <DropdownMenu>
-                        <DropdownMenuTrigger className="rounded-full">
-                            <Avatar className="w-9 h-9 ring-1 ring-muted-foreground">
-                                <AvatarImage src={user?.picture} alt={user?.name} />
-                                <AvatarFallback><FaRegUser /></AvatarFallback>
-                            </Avatar>
-                        </DropdownMenuTrigger >
-                        <DropdownMenuContent className="mr-2 w-56">
-                            <DropdownMenuLabel className=" text-center">
-                                <div className="flex justify-center p-4">
-                                    <Avatar className="scale-150">
-                                        <AvatarImage src={user?.picture} alt={user?.name} />
-                                        <AvatarFallback><FaRegUser /></AvatarFallback>
-                                    </Avatar>
-                                </div>
-                                <span>
-                                    {user?.name}
-                                </span>
-                                <br />
-                                <span className="text-muted-foreground font-light">{user?.email}</span>
-                            </DropdownMenuLabel>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>
-                                <FaRotate className="mr-2 h-4 w-4" /><span>Todoを切り替え</span>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <FaPlus className="mr-2 h-4 w-4" /><span>新しいTodoを作成</span>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <FaTrash className="mr-2 h-4 w-4" /><span>現在のTodoを削除</span>
-                            </DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                            <DropdownMenuItem>
-                                <FaGear className="mr-2 h-4 w-4" /><span>設定</span>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem onClick={_ => logout({ logoutParams: { returnTo: process.env.NEXT_PUBLIC_DOMAIN } })}>
-                                <FaArrowRightFromBracket className="mr-2 h-4 w-4" /><span>ログアウト</span>
-                            </DropdownMenuItem>
-                        </DropdownMenuContent>
-                    </DropdownMenu >
+                    <div className="flex gap-2">
+                        {isUpdate ? (
+                            <Button onClick={_ => { }} size={"default"} className="bg-blue-500 text-white hover:bg-blue-600 gap-1">
+                                {isSave ? (
+                                    <Spinner className="p-1 w-6 h-6 fill-cyan-500" />
+                                ) : (
+                                    <CloudUpload className="scale-75" />
+                                )}
+                                保存する
+                            </Button>
+                        ) : (
+                            <Button onClick={_ => { }} size={"default"} variant={"outline"} className="gap-1" disabled><CircleCheck className="scale-75" />保存済み</Button>
+                        )}
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="rounded-full">
+                                <Avatar className="w-9 h-9 ring-1 ring-muted-foreground">
+                                    <AvatarImage src={user?.picture} alt={user?.name} />
+                                    <AvatarFallback><FaRegUser /></AvatarFallback>
+                                </Avatar>
+                            </DropdownMenuTrigger >
+                            <DropdownMenuContent className="mr-2 w-56">
+                                <DropdownMenuLabel className=" text-center">
+                                    <div className="flex justify-center p-4">
+                                        <Avatar className="scale-150">
+                                            <AvatarImage src={user?.picture} alt={user?.name} />
+                                            <AvatarFallback><FaRegUser /></AvatarFallback>
+                                        </Avatar>
+                                    </div>
+                                    <span>
+                                        {user?.name}
+                                    </span>
+                                    <br />
+                                    <span className="text-muted-foreground font-light">{user?.email}</span>
+                                </DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <FaGear className="mr-2 h-4 w-4" /><span>設定</span>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem onClick={_ => logout({ logoutParams: { returnTo: process.env.NEXT_PUBLIC_DOMAIN } })}>
+                                    <FaArrowRightFromBracket className="mr-2 h-4 w-4" /><span>ログアウト</span>
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu >
+                    </div>
                 )
             }
         </div >

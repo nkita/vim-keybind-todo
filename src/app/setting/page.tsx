96d403eb660f68442f1048899d1e847599c95079
nkita <nkitam7@gmail.com>
2024-08-01 20:59:44 +0900

    feat: 設定画面を一旦追加

---
+++ src/app/setting/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { Cross1Icon, CrossCircledIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+
+export default function Home() {
+
+  return (
+    <article className="h-screen bg-sky-50/50">
+      <div className={`px-20 py-10 w-full`}>
+        <div className="flex justify-between content-center py-10">
+          <span className="text-2xl">情報・設定</span>
+          <Link href={"/"}><CrossCircledIcon className="scale-150" /></Link>
+
+        </div>
+        <div className="p-2 ">
+          <ul>
+            <li>Auto Save</li>
+            <li>Auto Save</li>
+            <li>Auto Save</li>
+            <li>Auto Save</li>
+          </ul>
+        </div>
+      </div>
+    </article >
+  );
+}

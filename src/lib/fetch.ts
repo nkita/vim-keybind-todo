1a3cae0c849a220249f71e7cf3f1a74e5629a704
nkita <nkitam7@gmail.com>
2024-09-07 15:29:23 +0900

    fix: Exception処理

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -22,6 +22,6 @@ export const postFetch = (url: string, token: string, body: Object) => {
     }).then(res => res.json());
 }
 
-export const useFetchList = (id: string, token: string) => useSWR(token ? [`${process.env.NEXT_PUBLIC_API}/api/list${id ? "/" + id : ""}`, token] : null, ([url, token]) => getFetch(url, token))
+export const useFetchList = (id: string, token: string) => useSWRImmutable(token ? [`${process.env.NEXT_PUBLIC_API}/api/list${id ? "/" + id : ""}`, token] : null, ([url, token]) => getFetch(url, token))
 export const useFetchTodo = (list_id: string, token: string) => useSWR(token ? [`${process.env.NEXT_PUBLIC_API}/api/list${list_id ? "/" + list_id + "/todo" : ""}`, token] : null, ([url, token]) => getFetch(url, token))
 export const useFetchPostList = (body: Object, token: string) => postFetch(`${process.env.NEXT_PUBLIC_API}/api/list`, token, body)

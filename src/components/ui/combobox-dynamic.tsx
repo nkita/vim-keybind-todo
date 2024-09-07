ffc3ceebb12661aeaa01fae043467e2166f11a6f
nkita <nkitam7@gmail.com>
2024-09-07 14:03:05 +0900

    fix: モーダルで入力できるプロジェクトを修正

--- src/components/ui/combobox-dynamic.tsx
+++ src/components/ui/combobox-dynamic.tsx
@@ -42,6 +42,7 @@ const DynamicSearchSelect = forwardRef<HTMLInputElement, SearchSelectProps>(
             if (value !== undefined && value !== null) addItem(value)
             setVal(val)
         }
+
         return (
             <Combobox onChange={handleChange} value={val} immediate={true}>
                 <div className="relative mt-1">
@@ -50,8 +51,8 @@ const DynamicSearchSelect = forwardRef<HTMLInputElement, SearchSelectProps>(
                             tabIndex={tabIndex}
                             displayValue={(item: string) => item}
                             onChange={(event) => setQuery(event.target.value)} autoFocus={autoFocus} placeholder={placeholder}
-                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50" /> 
-                            {/* className="placeholder:text-sm/tight" /> */}
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50" />
+                        {/* className="placeholder:text-sm/tight" /> */}
                         <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
                             <ChevronsUpDown
                                 className="ml-2 shrink-0 opacity-500"
@@ -62,35 +63,33 @@ const DynamicSearchSelect = forwardRef<HTMLInputElement, SearchSelectProps>(
                     <ComboboxOptions
                         className={`${filteredItems.length === 1 && !query && "hidden"} absolute p-3 border border-input my-2 max-h-60 w-full overflow-auto rounded-md text-popover-foreground bg-popover z-50 shadow-lg text-sm`}>
                         <>
+                            {!filteredItems.includes(query) &&
+                                <ComboboxOption value={query} className={
+                                    `relative cursor-default select-none py-2 pl-4 pr-4  w-full  data-[focus]:bg-accent data-[focus]:text-accent-foreground data-[focus]:cursor-pointer rounded-md`
+                                }>
+                                    <div className='flex gap-1 items-center justify-between'>
+                                        <span className='font-bold truncate flex gap-1'>{query}</span><span className='text-xs text-muted-foreground'></span>
+                                        <Badge variant={"outline"} className='bg-emerald-500 text-white' >new</Badge>
+                                    </div>
+                                </ComboboxOption>
+                            }
                             {
-                                filteredItems.length <= 1 ? (
-                                    <>
-                                        <ComboboxOption value={query} className={
-                                            `relative cursor-default select-none py-2 px-2 data-[focus]:cursor-pointer rounded-md`
-                                        }>
-                                            <div className='flex gap-1 items-center'>
-                                                <Badge variant={"outline"} className='bg-emerald-500 text-white' >new</Badge>
-                                                <span className='font-bold truncate flex gap-1'>{query}</span><span className='text-xs text-muted-foreground'></span>
-                                            </div>
-                                        </ComboboxOption>
-                                    </>
-                                ) : (
-                                    filteredItems.map((item: any) => {
-                                        return (
-                                            <div key={item} className={`relative flex items-center`}>
-                                                <ComboboxOption value={item} className={
-                                                    `relative cursor-default select-none py-2 pl-4 pr-4  w-full  data-[focus]:bg-accent data-[focus]:text-accent-foreground data-[focus]:cursor-pointer rounded-md`
-                                                }>
-                                                    <div className='flex justify-between items-center'>
-                                                        <div className={`flex justify-between align-middle font-bold items-center w-full ${item === "" && "text-red-500"}`}>
-                                                            {item === "" ? "削除" : item}
-                                                        </div>
+                                filteredItems.map((item: any) => {
+                                    return (
+                                        <div key={item} className={`relative flex items-center`}>
+                                            <ComboboxOption value={item} className={
+                                                `relative cursor-default select-none py-2 pl-4 pr-4  w-full  data-[focus]:bg-accent data-[focus]:text-accent-foreground data-[focus]:cursor-pointer rounded-md`
+                                            }>
+                                                <div className='flex justify-between items-center'>
+                                                    <div className={`flex justify-between align-middle font-bold items-center w-full ${item === "" && "text-red-500"}`}>
+                                                        {item === "" ? "削除" : item}
                                                     </div>
-                                                </ComboboxOption>
-                                            </div>
-                                        )
-                                    })
-                                )}
+                                                </div>
+                                            </ComboboxOption>
+                                        </div>
+                                    )
+                                })
+                            }
                         </>
                     </ComboboxOptions>
                 </div>

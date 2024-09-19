faa5e8839e1c3b6eb44b8e9cfae47283113a38c5
nkita <nkitam7@gmail.com>
2024-09-19 20:28:50 +0900

    fix: デザイン微調整

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AuthProvider } from "@/provider/auth";
-import { Zen_Kaku_Gothic_New } from "next/font/google";
+// import { Zen_Kaku_Gothic_New } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner"
+// const inter = Zen_Kaku_Gothic_New({ weight: "400", subsets: ["latin"] })
 
-const inter = Zen_Kaku_Gothic_New({ weight: "400", subsets: ["latin"] })
+import { Noto_Sans_JP } from "next/font/google";
+
+const noto = Noto_Sans_JP({
+  weight: ["400"],
+  style: "normal",
+  subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
   title: "キーボードでToDo| ショートカットを駆使した爆速Todoアプリ",
@@ -19,7 +27,7 @@ export default function RootLayout({
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={noto.className}>
           {children}
           <Toaster position="top-center" richColors />
         </body>

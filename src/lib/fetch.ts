9c924ff023f41645080ff0c75fbd1426c3315352
nkita <nkitam7@gmail.com>
2024-12-04 23:02:51 +0900

    feat(me): アクティビティ& My Projects修正

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -27,3 +27,5 @@ export const useFetchTodo = (list_id: string | null, token: string | null) => us
 export const useFetchCompletedTodo = (list_id: string | null, page: number = 1, token: string | null) => useSWR(token && list_id ? [`${process.env.NEXT_PUBLIC_API}/api/list${list_id ? "/" + list_id + "/todo?completionOnly=true" : ""}`, token] : null, ([url, token]) => getFetch(url, token))
 export const useFetchPostList = (body: Object, token: string | null) => postFetch(`${process.env.NEXT_PUBLIC_API}/api/list`, token, body)
 export const useFetchSummary = (token: string | null) => useSWR(token ? [`${process.env.NEXT_PUBLIC_API}/api/summary`, token] : null, ([url, token]) => getFetch(url, token))
+export const useFetchActivity = (token: string | null, year: string) => useSWR(token && year ? [`${process.env.NEXT_PUBLIC_API}/api/summary/${year}`, token] : null, ([url, token]) => getFetch(url, token))
+export const useFetchTimeline = (token: string | null) => useSWR(token ? [`${process.env.NEXT_PUBLIC_API}/api/timeline?page=1&limit=10`, token] : null, ([url, token]) => getFetch(url, token))
